4669cd26b0aa3bb2e639023dd2bec35a886c1561
Calvin <c.bauer@nectarpdx.com>
2021-02-03 18:38:51 -0800

    Finished

--- Leafleaf-Step-1/static/js/logic.js
+++ Leafleaf-Step-1/static/js/logic.js
@@ -18,15 +18,11 @@ d3.json(queryUrl).then(function (data) {
     return L.circleMarker(latlng, geojsonMarkerOptions);
   }
 
-
-  // This is it! Leaflet knows what to do with 
-  // each type of feature (held in the `geometry` key) and draws the correct markers.
   var earthquakes = L.geoJSON(data.features, {
     onEachFeature: onEachFeatureFunc,
     pointToLayer: pointToLayerFunc
   });
 
-  // The rest of this is all the same
   var worldmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -62,12 +58,12 @@ d3.json(queryUrl).then(function (data) {
   });
 
   // Create a layer control
-  // Pass in our baseMaps and overlayMaps
-  // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
 
+
+  // Adding Legend
   var legend = L.control({ position: 'bottomright' });
 
   legend.onAdd = function (map) {
@@ -90,19 +86,18 @@ d3.json(queryUrl).then(function (data) {
 
 });
 
-// https://leafletjs.com/examples/geojson/
-// L.geoJSON() also gives us handy options, almost like a built in `.forEach()`
-// // Define a function we want to run once for each feature in the features array
-// // Give each feature a popup describing the place and time of the earthquake
+// Words inside of Markers
 function onEachFeatureFunc(feature, layer) {
   layer.bindPopup("<h3>" + new Date(feature.properties.time) +
-    "</h3> <hr><p>" + (feature.properties.place) + "</p><p>" + feature.properties.mag + "</p>" + "<p>" + feature.geometry.coordinates[2] + "</p>");
+    "</h3> <hr><p>" + "Location: " + (feature.properties.place) + "</p><p>" + "Magnitude: " + feature.properties.mag + "</p>" + "<p>" + "Depth: " +feature.geometry.coordinates[2] + "</p>");
 }
 
-
+// Marker Sizes
 function magnitudeRadius(eq) {
   return eq * 6.5;
 }
+
+// Marker Colors
 function depthColor(depth) {
   return depth >= 90 ? '#800026' :
     depth >= 70 ? '#BD0026' :
