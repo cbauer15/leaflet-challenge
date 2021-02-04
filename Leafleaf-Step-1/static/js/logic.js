f409c4ac13b29a11c710f2d5732a115488fe54f8
Calvin <c.bauer@nectarpdx.com>
2021-02-03 18:01:05 -0800

    almost done

--- Leafleaf-Step-1/static/js/logic.js
+++ Leafleaf-Step-1/static/js/logic.js
@@ -6,6 +6,19 @@ d3.json(queryUrl).then(function (data) {
   // The data.features object is in the GeoJSON standard
   console.log(data.features);
 
+  function pointToLayerFunc(feature, latlng) {
+    var geojsonMarkerOptions = {
+      radius: magnitudeRadius(feature.properties.mag),
+      fillColor: depthColor(feature.geometry.coordinates[2]),
+      color: "#000",
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+    return L.circleMarker(latlng, geojsonMarkerOptions);
+  }
+
+
   // This is it! Leaflet knows what to do with 
   // each type of feature (held in the `geometry` key) and draws the correct markers.
   var earthquakes = L.geoJSON(data.features, {
@@ -55,6 +68,26 @@ d3.json(queryUrl).then(function (data) {
     collapsed: false
   }).addTo(myMap);
 
+  var legend = L.control({ position: 'bottomright' });
+
+  legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [10, 30, 50, 70, 90],
+      labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + depthColor(grades[i] + 1) + '"></i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+  };
+
+  legend.addTo(myMap);
+
 });
 
 // https://leafletjs.com/examples/geojson/
@@ -62,18 +95,19 @@ d3.json(queryUrl).then(function (data) {
 // // Define a function we want to run once for each feature in the features array
 // // Give each feature a popup describing the place and time of the earthquake
 function onEachFeatureFunc(feature, layer) {
-  layer.bindPopup("<h3>" + feature.properties.place +
-    "</h3><hr><p>" + new Date(feature.properties.time) + "</p><p>" + feature.properties.mag + "</p>");
+  layer.bindPopup("<h3>" + new Date(feature.properties.time) +
+    "</h3> <hr><p>" + (feature.properties.place) + "</p><p>" + feature.properties.mag + "</p>" + "<p>" + feature.geometry.coordinates[2] + "</p>");
 }
-var geojsonMarkerOptions = {
-  radius: 8,
-  fillColor: "#ff7800",
-  color: "#000",
-  weight: 1,
-  opacity: 1,
-  fillOpacity: 0.8
-};
 
-function pointToLayerFunc(feature, latlng) {
-  return L.circleMarker(latlng, geojsonMarkerOptions);
+
+function magnitudeRadius(eq) {
+  return eq * 6.5;
 }
+function depthColor(depth) {
+  return depth >= 90 ? '#800026' :
+    depth >= 70 ? '#BD0026' :
+      depth >= 50 ? '#E31A1C' :
+        depth >= 30 ? '#FC4E2A' :
+          depth >= 10 ? '#FD8D3C' :
+            '#FFEDA0';
+};
